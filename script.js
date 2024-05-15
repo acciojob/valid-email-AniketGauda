17159e4336d9384f855bc906e20c55d4a29624dc
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-15 08:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function validEmail(str) {
   //your JS code here.
+	if(str==null || !str.includes('@') )
+        return false;
+    else{
+        let a = str.split('@');
+        if(a.length!=2)
+            return false;
+        else{
+            if(a[0].includes(".") || a[0].length==0)
+                return false;
+            else{
+                let b = a[1].split(".");
+                if(b.length<2)
+                    return false;
+                else{
+                    if(b[0]==null || b[1]==null)
+                        return false;
+                    else
+                        return true;
+                }
+            }
+        }
+    }
 }
 
 // Do not change the code below.
